4cb13d8d085db0a3f29115238ab187117fb2dd4a
Choiwonwong <chldnjsdnd12@naver.com>
2023-09-20 15:12:29 +0900

    0920 detail Update

--- API/main.py
+++ API/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from .routes import requests, quests
 
 app = FastAPI()
-app.include_router(requests.router, prefix='/requests')
+app.include_router(requests.router, prefix='/api/requests')
 app.include_router(quests.build_router, prefix='/build')
 app.include_router(quests.provision_router, prefix='/provision')
 app.include_router(quests.deploy_router, prefix='/deploy')
