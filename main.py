7371fbedc23f81b03b79f1f7dc709e278d9ea964
Choiwonwong <chldnjsdnd12@naver.com>
2023-10-10 09:22:45 +0900

    Load KubeConfig

--- main.py
+++ main.py
@@ -1,17 +1,32 @@
 from fastapi import FastAPI
 # from .routes import requests, quests
 from connection import Base, engine
-
+from kubernetes import client, config
 
 Base.metadata.create_all(bind=engine)
+
+config.load_kube_config()
+api_client = client.CoreV1Api()
 
 app = FastAPI()
 
 @app.get('/')
-async def home():
-    return {
-        "message": "Hello"
-    }
+async def get_namespaces():
+    try:
+        # Get the list of namespaces
+        namespaces = api_client.list_namespace()
+        
+        # Extract the namespace names
+        namespace_names = [namespace.metadata.name for namespace in namespaces.items]
+        
+        return {
+            "message": "List of Kubernetes namespaces",
+            "namespaces": namespace_names
+        }
+    except Exception as e:
+        return {
+            "error": f"An error occurred: {str(e)}"
+        }
 
 # Dependency
 # def get_db():
